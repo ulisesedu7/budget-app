7d3db68631b418823852a3efaa75bda28a40fb60
Ulises Largaespada <ulisesedu7@users.noreply.github.com>
2022-10-26 17:18:07 -0600

    Set up rails app with initial database settings

---
+++ db/migrate/20221026231238_create_payments.rb
@@ -0,0 +1,11 @@
+class CreatePayments < ActiveRecord::Migration[7.0]
+  def change
+    create_table :payments do |t|
+      t.integer :author_id
+      t.string :name
+      t.float :amount
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/views/payments/new.html.erb_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "payments/new", type: :view do
+  before(:each) do
+    assign(:payment, Payment.new(
+      author_id: 1,
+      name: "MyString",
+      amount: 1.5
+    ))
+  end
+
+  it "renders new payment form" do
+    render
+
+    assert_select "form[action=?][method=?]", payments_path, "post" do
+
+      assert_select "input[name=?]", "payment[author_id]"
+
+      assert_select "input[name=?]", "payment[name]"
+
+      assert_select "input[name=?]", "payment[amount]"
+    end
+  end
+end
