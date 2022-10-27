d8395250627c6d541780a89da19b48297c313412
Ulises Largaespada <ulisesedu7@users.noreply.github.com>
2022-10-26 20:28:04 -0600

    Add devise to aunthenticate the users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
+
   # Data Relationships
   has_many :categories, foreign_key: 'user_id', dependent: :destroy
   has_many :payments, foreign_key: 'user_id', dependent: :destroy
