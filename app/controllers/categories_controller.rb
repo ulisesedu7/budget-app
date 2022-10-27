bafc8dae166e0a66c00df023a24dd9a797220661
Ulises Largaespada <ulisesedu7@users.noreply.github.com>
2022-10-27 16:52:32 -0600

    Finish Catefories functionalities and styles

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,9 +1,11 @@
 class CategoriesController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_category, only: %i[ show edit update destroy ]
 
   # GET /categories or /categories.json
   def index
-    @categories = Category.all
+    @current_user = current_user
+    @categories = @current_user.categories
   end
 
   # GET /categories/1 or /categories/1.json
@@ -21,11 +23,12 @@ class CategoriesController < ApplicationController
 
   # POST /categories or /categories.json
   def create
-    @category = Category.new(category_params)
+    @user = current_user
+    @category = @current_user.categories.create(category_params)
 
     respond_to do |format|
       if @category.save
-        format.html { redirect_to category_url(@category), notice: "Category was successfully created." }
+        format.html { redirect_to categories_path, notice: "Category was successfully created." }
         format.json { render :show, status: :created, location: @category }
       else
         format.html { render :new, status: :unprocessable_entity }
